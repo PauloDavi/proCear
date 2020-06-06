9ed623dc3777b8e583a7a02839fec2205d9b586e
Paulo Davi <paulodavizinho69@gmail.com>
2020-06-06 13:21:33 -0300

    test(backend): test a backend commit lint

--- backend/commitlint.config.js
+++ backend/commitlint.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional']
-  // rules: {
-  //   'body-max-line-length': [2, 'always', 50],
-  //   'scope-enum': [2, 'always', ['backend', 'frontend', 'mobile']],
-  // },
 };
